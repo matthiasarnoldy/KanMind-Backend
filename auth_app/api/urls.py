3b609d2a493b9776d52c865ee5cd3c0db92ce768
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-07-07 16:05:47 +0200

    feat: replace CustomLoginView with EmailLoginView in URLs and views

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import RegistrationView, CustomLoginView
+from .views import RegistrationView, EmailLoginView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', EmailLoginView.as_view(), name='login'),
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -28,7 +28,7 @@ class RegistrationView(APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class CustomLoginView(APIView):
+class EmailLoginView(APIView):
     authentication_classes = []
     permission_classes = [AllowAny]
 
