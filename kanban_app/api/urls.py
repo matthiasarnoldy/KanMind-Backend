1bd16cf1c2f81abe1630163f3c07e79de75cd5e5
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-07-09 17:00:43 +0200

    feat: define URL patterns for BoardListCreateView and BoardDetailView

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-# from .views import 
+from .views import BoardListCreateView, BoardDetailView
 
-urlpatterns = []
+urlpatterns = [
+    path('boards/', BoardListCreateView.as_view(), name="board-list-create"),
+    path('boards/<int:board_id>', BoardDetailView.as_view(), name="board-detail"),
+    path('', ),
+]
